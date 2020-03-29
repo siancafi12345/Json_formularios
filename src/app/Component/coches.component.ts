b4e1d528258903e52948bbd4779d714b61527c8a
siancafi1 <sacastanedafr@ut.edu.co>
2020-03-29 17:15:55 -0500

    Formulario

--- src/app/Component/coches.component.ts
+++ src/app/Component/coches.component.ts
@@ -7,11 +7,22 @@ import { CochesdoModel } from "../Model/coche"
 })
 export class CochesComponent{
     public coche: CochesdoModel;
+    public coches:Array<CochesdoModel>;
 
     constructor(){
         this.coche = new CochesdoModel("","","");
+        this.coches =[
+            new CochesdoModel("carro","1200","blanco"),
+            new CochesdoModel("moto","125","blanco")
+        ]
     }
     onSubmit(){
-        console.log(this.coche);        
+        if(this.coches.push(this.coche)){
+            this.coche =  new CochesdoModel("","","");
+        }    
+        return this.coches, this.coche;
+    }
+    eliminar(i){
+        this.coches.splice(i,1);
     }
 }
